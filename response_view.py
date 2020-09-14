9d355ba9d5aad59da9189fc4dfff57867af26240
Salmansoori <64367505+Salmansoori@users.noreply.github.com>
2020-09-14 17:25:57 +0530

    Create response_view.py

---
+++ response_view.py
@@ -0,0 +1,18 @@
+
+@csrf_exempt
+def handlerequest(request):
+    # paytm will send you post request here
+    form = request.POST
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('order successful')
+        else:
+            print('order was not successful because' + response_dict['RESPMSG'])
+    return render(request, 'shop/paymentstatus.html', {'response': response_dict})

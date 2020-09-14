b46956cb73eebf04c5b62d7067d685943602076a
Salmansoori <64367505+Salmansoori@users.noreply.github.com>
2020-09-14 17:23:13 +0530

    Create checkout_part.py

---
+++ checkout_part.py
@@ -0,0 +1,14 @@
+ param_dict = {
+
+                'MID': "your merchant id",
+                'ORDER_ID': str(order.order_id),
+                'TXN_AMOUNT': str(amount),
+                'CUST_ID': email,
+                'INDUSTRY_TYPE_ID': 'Retail',
+                'WEBSITE': 'WEBSTAGING',
+                'CHANNEL_ID': 'WEB',
+                'CALLBACK_URL':'http://127.0.0.1:8000/response/',
+
+        }
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+        return render(request, 'paytm.html', {'param_dict': param_dict})
